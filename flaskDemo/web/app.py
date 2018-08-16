416e2b4d7ad4edf71be701b44463683cec343a5b
Matt <mcamack@gmail.com>
2018-08-15 21:58:32 -0700

    numpy endpoint working

--- flaskDemo/web/app.py
+++ flaskDemo/web/app.py
@@ -1,27 +1,46 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
-import json
+import json, codecs
+import numpy as np
+import pandas as pd
+
 app = Flask(__name__)
 
+# Flask Routes
 @app.route('/postjson', methods=['POST'])
 def post_json_handler():
-    print (request.is_json)
     content = request.get_json()
-    print (content)
+    return parse_types(content)
 
-    ret = parse_json(content)
-    return ret
+@app.route('/numpy', methods=['POST'])
+def create_numpy():
+    content = request.get_json()
+    return parse_numpy(content)
 
-def parse_json(json_data):
+# Processing
+def parse_numpy(data):
+    mylist = []
+
+    #parse each number from the JSON data
+    for value in data:
+        mylist.append(value*10)
+
+    array = np.array(mylist)
+    return pd.Series(array).to_json(orient='values')
+
+
+def parse_types(data):
     ret = {}
     
-    for key, value in json_data.items():
+    # parse each key/value pair from the JSON data
+    for key, value in data.items():
         if type(value) is int:
             ret[key] = value * 100
         elif type(value) is str:
             ret[key] = value + " is cool!"
 
     return json.dumps(ret)
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
