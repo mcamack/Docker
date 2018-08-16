0946e03683a8f4fc135ff4463eda2076d3800ba5
Matt <mcamack@gmail.com>
2018-08-15 20:24:31 -0700

    parses json, ret new json

--- flaskDemo/web/app.py
+++ flaskDemo/web/app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
+import json
 app = Flask(__name__)
 
 @app.route('/postjson', methods=['POST'])
@@ -7,8 +8,20 @@ def post_json_handler():
     print (request.is_json)
     content = request.get_json()
     print (content)
-    return jsonify('JSON posted')
 
+    ret = parse_json(content)
+    return ret
+
+def parse_json(json_data):
+    ret = {}
+    
+    for key, value in json_data.items():
+        if type(value) is int:
+            ret[key] = value * 100
+        elif type(value) is str:
+            ret[key] = value + " is cool!"
+
+    return json.dumps(ret)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
