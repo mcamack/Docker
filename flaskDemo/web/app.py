9065365e8e4baf451cea51f906f861fdc53e4df8
Matt <mcamack@gmail.com>
2018-08-16 21:16:27 -0700

    dataframe to_datetime

--- flaskDemo/web/app.py
+++ flaskDemo/web/app.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
 import json, codecs
 import numpy as np
@@ -9,25 +8,22 @@ app = Flask(__name__)
 # Flask Routes
 @app.route('/postjson', methods=['POST'])
 def post_json_handler():
-    content = request.get_json()
-    return parse_types(content)
+    data = request.get_json()
+    return parse_types(data)
 
 @app.route('/numpy', methods=['POST'])
 def create_numpy():
-    content = request.get_json()
-    return parse_numpy(content)
+    data = request.get_json()
+    return parse_numpy(data)
 
 # Processing
 def parse_numpy(data):
     mylist = []
-
-    #parse each number from the JSON data
-    for value in data:
-        mylist.append(value*10)
-
-    array = np.array(mylist)
-    return pd.Series(array).to_json(orient='values')
-
+    for row in data:
+        mylist.append(row)
+    df = pd.DataFrame(mylist)
+    df[0] = pd.to_datetime(df[0])
+    return df.to_json(orient='values')
 
 def parse_types(data):
     ret = {}
